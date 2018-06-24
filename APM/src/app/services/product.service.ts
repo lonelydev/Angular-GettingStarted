3779f99b7e3c802a880c8bd281b04d5b136f7dfe
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-06-24 16:53:22 +0100

    feat(product-details): Page completed.
    New servcie endpoint to return product information for an identifier.

--- APM/src/app/services/product.service.ts
+++ APM/src/app/services/product.service.ts
@@ -5,18 +5,25 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  private _productUrl = '../../api/products/products.json';
-  constructor(private _http: HttpClient) { }
-  getProducts(): Observable<IProduct[]> {
-    return this._http.get<IProduct[]>(this._productUrl)
-    .do(data => console.log('All' + JSON.stringify(data)))
-    .catch(this.handleError);
-  }
-  private handleError(err: HttpErrorResponse) {
-    console.log(err.message);
-    return Observable.throw(err.message);
-  }
+    private _productUrl = '../../api/products/products.json';
+    constructor(private _http: HttpClient) {}
+    getProducts(): Observable<IProduct[]> {
+        return this._http
+            .get<IProduct[]>(this._productUrl)
+            .do((data) => console.log('All' + JSON.stringify(data)))
+            .catch(this.handleError);
+    }
+
+    getProduct(id: number): Observable<IProduct> {
+        return this.getProducts().map((pList) => pList.filter((p) => p.productId === id)[0]).catch(this.handleError);
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        console.log(err.message);
+        return Observable.throw(err.message);
+    }
 }
