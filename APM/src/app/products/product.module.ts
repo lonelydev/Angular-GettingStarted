f40d433fa704b929a4de189e3498eceead105ad7
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2018-06-26 22:49:15 +0100

    feat(sharedModule): Shared Module
    Further refactoring the app to segregated shared functionality.

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,31 +1,27 @@
 import { ConvertToSpacesPipe } from './../shared/convert-to-spaces.pipe';
 import { ProductListComponent } from './product-list.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './product-detail.component';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductGuardService } from './product-guard.service';
 import { ProductService } from '../services/product.service';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id',
       canActivate: [ ProductGuardService ],
       component: ProductDetailComponent }
     ]),
+    SharedModule,
   ],
   providers: [ ProductService, ProductGuardService ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    ConvertToSpacesPipe
   ],
 })
 export class ProductModule { }
